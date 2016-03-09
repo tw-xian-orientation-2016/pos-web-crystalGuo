488a0aeda3ca989bfa4efda5153442a36f03dc5a
crystal <793832732@qq.com>
2016-03-09 17:18:20 +0800

    add initialList.js

---
+++ main/initialList.js
@@ -0,0 +1,28 @@
+$(document).ready(initialList);
+
+function initialList() {
+
+  if (localStorage.getItem("list") === null) {
+    localStorage.setItem("list", JSON.stringify(loadAllItems()));
+  }
+
+  var list = JSON.parse(localStorage.getItem("list"));
+
+  createTable(list);
+}
+
+function createTable(list) {
+
+  for (var i = 0; i < list.length; i++)
+     {
+        var tr=$("<tr></tr>");
+        tr.appendTo($("#listTable"));
+
+        var td = $("<td>" + list[i].name + "</td>");
+        td.appendTo(tr);
+        td = $("<td>" + list[i].price.toFixed(2) + "/" + list[i].unit + "</td>");
+        td.appendTo(tr);
+        td = $("<td>" + "<input type = 'button', name = 'addButton', data-id = list[i].barcode, value = 'addToCart'/>" + "</td>");
+        td.appendTo(tr);
+     }
+}
